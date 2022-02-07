c70d3a351c3bb4f259d5ce3cff835c6fc2e802ab
seondongpyo <seondongpyo@gmail.com>
2022-02-07 20:46:43 +0900

    initial security commit

---
+++ security/src/test/java/io/github/seondongpyo/security/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.seondongpyo.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
