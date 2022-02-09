553da1c088763401bf6203b72bac16a986be2bfb
seondongpyo <seondongpyo@gmail.com>
2022-02-09 20:43:13 +0900

    feat: 회원 도메인 추가

---
+++ mybatis/src/main/java/io/github/seondongpyo/mybatis/domain/Member.java
@@ -0,0 +1,13 @@
+package io.github.seondongpyo.mybatis.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Member {
+
+	private Long id;
+	private String name;
+	private int age;
+}
