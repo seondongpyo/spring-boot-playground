ddcd4a37c38dc517d2ce4660c01958bd99e9e3bc
seondongpyo <seondongpyo@gmail.com>
2022-02-10 20:48:18 +0900

    docs: Post table ddl 추가

--- mybatis/src/main/resources/db/init.sql
+++ mybatis/src/main/resources/db/init.sql
@@ -1,4 +1,5 @@
 DROP TABLE IF EXISTS Member;
+DROP TABLE IF EXISTS Post;
 
 CREATE TABLE Member
 (
@@ -9,3 +10,12 @@ CREATE TABLE Member
 
 INSERT INTO Member VALUES(1, '홍길동', 20);
 INSERT INTO Member VALUES(2, '김길동', 30);
+
+CREATE TABLE Post
+(
+    id      bigint primary key auto_increment,
+    writer  varchar(255),
+    title   varchar(255),
+    content clob
+);
+
