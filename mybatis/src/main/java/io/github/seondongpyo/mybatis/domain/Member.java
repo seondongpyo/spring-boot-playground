7acaee6c7dd596fa2ee052a13b2305496833b66e
seondongpyo <seondongpyo@gmail.com>
2022-02-10 20:23:29 +0900

    fix: `@AllArgsConstructor` 어노테이션 추가

--- mybatis/src/main/java/io/github/seondongpyo/mybatis/domain/Member.java
+++ mybatis/src/main/java/io/github/seondongpyo/mybatis/domain/Member.java
@@ -1,10 +1,12 @@
 package io.github.seondongpyo.mybatis.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class Member {
 
 	private Long id;
