3758807d3958afbfc9981132be9fc509f3833336
seondongpyo <seondongpyo@gmail.com>
2022-02-13 23:13:05 +0900

    feat: 사용자 도메인 추가

---
+++ security/src/main/java/io/github/seondongpyo/security/domain/User.java
@@ -0,0 +1,18 @@
+package io.github.seondongpyo.security.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String username;
+
+	private String password;
+}
