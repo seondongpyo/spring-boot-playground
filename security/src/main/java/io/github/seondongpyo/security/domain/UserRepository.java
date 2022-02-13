0d926054af4c87f0b68016f0f50bc6db2c06bd90
seondongpyo <seondongpyo@gmail.com>
2022-02-13 23:26:18 +0900

    feat: 사용자 리포지토리 추가

---
+++ security/src/main/java/io/github/seondongpyo/security/domain/UserRepository.java
@@ -0,0 +1,8 @@
+package io.github.seondongpyo.security.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+	User findByUsername(String username);
+}
