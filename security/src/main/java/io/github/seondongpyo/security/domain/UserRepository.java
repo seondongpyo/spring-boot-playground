65fb8724716e6251db593a3a793925404139ae81
seondongpyo <seondongpyo@gmail.com>
2022-02-13 23:29:35 +0900

    refactor: 사용자 조회 시 반환 타입을 Optional로 변경

--- security/src/main/java/io/github/seondongpyo/security/domain/UserRepository.java
+++ security/src/main/java/io/github/seondongpyo/security/domain/UserRepository.java
@@ -1,8 +1,10 @@
 package io.github.seondongpyo.security.domain;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
-	User findByUsername(String username);
+	Optional<User> findByUsername(String username);
 }
