42280c1fba23c72026ffcbeddfe1d411b97cfa3e
seondongpyo <seondongpyo@gmail.com>
2022-02-13 23:30:02 +0900

    feat: 사용자 서비스 추가

---
+++ security/src/main/java/io/github/seondongpyo/security/domain/UserService.java
@@ -0,0 +1,21 @@
+package io.github.seondongpyo.security.domain;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
+public class UserService implements UserDetailsService {
+
+	private final UserRepository userRepository;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		return userRepository.findByUsername(username)
+				.orElseThrow(() -> new UsernameNotFoundException("User '" + username + "' not found"));
+	}
+}
