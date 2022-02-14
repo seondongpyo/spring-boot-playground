8c84ce9b682f66125c25c3c9fd4ebdd1d65c5adf
seondongpyo <seondongpyo@gmail.com>
2022-02-14 19:46:11 +0900

    refactor: 시큐리티 설정 추가

--- security/src/main/java/io/github/seondongpyo/security/config/SecurityConfig.java
+++ security/src/main/java/io/github/seondongpyo/security/config/SecurityConfig.java
@@ -1,13 +1,35 @@
 package io.github.seondongpyo.security.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
+import io.github.seondongpyo.security.domain.UserService;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	private final UserService userService;
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth
+			.userDetailsService(userService)
+			.passwordEncoder(passwordEncoder());
+	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
@@ -18,15 +40,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 				.disable()
 			.and()
 				.authorizeRequests()
-				.antMatchers("/", "/login")
+				.antMatchers("/login")
 					.permitAll()
 				.anyRequest()
 					.authenticated()
 			.and()
 				.formLogin()
-				.loginPage("/login")
-				.loginProcessingUrl("/login")
-				.defaultSuccessUrl("/")
-				.failureUrl("/login?error=true");
+					.loginPage("/login")
+					.loginProcessingUrl("/login")
+					.defaultSuccessUrl("/")
+					.failureUrl("/login?error=true");
 	}
 }
