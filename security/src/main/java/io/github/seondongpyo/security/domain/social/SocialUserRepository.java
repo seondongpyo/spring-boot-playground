250816af8f5eae892f6021199e7496dcd8096928
seondongpyo <seondongpyo@gmail.com>
2022-02-16 23:46:06 +0900

    feat: 소셜 로그인 사용자용 리포지토리 추가

---
+++ security/src/main/java/io/github/seondongpyo/security/domain/social/SocialUserRepository.java
@@ -0,0 +1,10 @@
+package io.github.seondongpyo.security.domain.social;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface SocialUserRepository extends JpaRepository<SocialUser, Long> {
+
+    Optional<SocialUser> findByEmail(String email);
+}
