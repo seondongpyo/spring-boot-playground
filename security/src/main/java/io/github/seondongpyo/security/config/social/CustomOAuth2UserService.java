3280a05658771a9aeda621118ba3d9d4f8e9fe12
seondongpyo <seondongpyo@gmail.com>
2022-02-17 00:05:46 +0900

    feat: 소셜 로그인 사용자의 정보를 기반으로 가입, 수정 및 세션 저장을 처리할 클래스 추가

---
+++ security/src/main/java/io/github/seondongpyo/security/config/social/CustomOAuth2UserService.java
@@ -0,0 +1,58 @@
+package io.github.seondongpyo.security.config.social;
+
+import io.github.seondongpyo.security.domain.social.SocialUser;
+import io.github.seondongpyo.security.domain.social.SocialUserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpSession;
+import java.util.Collections;
+
+@RequiredArgsConstructor
+@Service
+public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
+
+    private final SocialUserRepository socialUserRepository;
+    private final HttpSession httpSession;
+
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+        DefaultOAuth2UserService delegate = new DefaultOAuth2UserService();
+        OAuth2User oAuth2User = delegate.loadUser(userRequest);
+
+        String registrationId = userRequest
+                .getClientRegistration()
+                .getRegistrationId();
+
+        String userNameAttributeName = userRequest
+                .getClientRegistration()
+                .getProviderDetails()
+                .getUserInfoEndpoint()
+                .getUserNameAttributeName();
+
+        OAuthAttributes attributes = OAuthAttributes.of(registrationId, userNameAttributeName, oAuth2User.getAttributes());
+
+        SocialUser user = saveOrUpdate(attributes);
+        httpSession.setAttribute("user", new SessionUser(user));
+
+        return new DefaultOAuth2User(
+            Collections.singleton(new SimpleGrantedAuthority(user.getRoleKey())),
+            attributes.getAttributes(),
+            attributes.getNameAttributeKey()
+        );
+    }
+
+    private SocialUser saveOrUpdate(OAuthAttributes attributes) {
+        SocialUser user = socialUserRepository.findByEmail(attributes.getEmail())
+                .map(entity -> entity.update(attributes.getName(), attributes.getPicture()))
+                .orElse(attributes.toEntity());
+        return socialUserRepository.save(user);
+    }
+}
