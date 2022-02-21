8fa7e806e97d256cc3201c5b1ccf71cbbef5acf6
seondongpyo <seondongpyo@gmail.com>
2022-02-22 00:14:41 +0900

    feat: 로그인 세션 처리

--- security/src/main/java/io/github/seondongpyo/security/web/IndexController.java
+++ security/src/main/java/io/github/seondongpyo/security/web/IndexController.java
@@ -1,17 +1,38 @@
 package io.github.seondongpyo.security.web;
 
+import io.github.seondongpyo.security.config.social.SessionUser;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import javax.servlet.http.HttpSession;
+
+@RequiredArgsConstructor
+@Controller
 public class IndexController {
 
+	private final HttpSession httpSession;
+
+	@GetMapping("/")
+	public String index(Model model) {
+		SessionUser user = (SessionUser) httpSession.getAttribute("user");
+		if (user != null) {
+			model.addAttribute("username", user.getName());
+		}
+		return "index";
+	}
+
 	@GetMapping("/user")
+	@ResponseBody
 	public String user() {
 		return "If you can see this message, your role is User";
 	}
 
 	@GetMapping("/admin")
+	@ResponseBody
 	public String admin() {
 		return "If you can see this message, your role is Admin";
 	}
