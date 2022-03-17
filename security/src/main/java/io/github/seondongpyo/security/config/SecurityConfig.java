7684944ba291b6be18e93803e8f090b621817d6d
seondongpyo <seondongpyo@gmail.com>
2022-03-17 20:48:15 +0900

    feat: 정적 리소스 보안 설정 추가

--- security/src/main/java/io/github/seondongpyo/security/config/SecurityConfig.java
+++ security/src/main/java/io/github/seondongpyo/security/config/SecurityConfig.java
@@ -3,9 +3,11 @@ package io.github.seondongpyo.security.config;
 import io.github.seondongpyo.security.config.social.CustomOAuth2UserService;
 import io.github.seondongpyo.security.domain.Role;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -16,6 +18,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	private final CustomOAuth2UserService customOAuth2UserService;
 	private final CustomAuthenticationProvider authenticationProvider;
+
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		// 해당 설정 추가 시, 정적 리소스들은 보안 필터를 거치지 않는다.
+		web
+			.ignoring()
+			.requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+	}
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
