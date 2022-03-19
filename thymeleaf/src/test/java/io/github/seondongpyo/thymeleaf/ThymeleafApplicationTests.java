513ef24744afd65b1758fd41c2b224297c4a5e56
seondongpyo <seondongpyo@gmail.com>
2022-03-19 22:10:34 +0900

    feat: create a project

---
+++ thymeleaf/src/test/java/io/github/seondongpyo/thymeleaf/ThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.seondongpyo.thymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
