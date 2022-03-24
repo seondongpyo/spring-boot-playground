b469c5b38f8dbb9ca9060110b1b2ed3ca844552c
seondongpyo <seondongpyo@gmail.com>
2022-03-25 00:14:05 +0900

    feat: 유효성 검증 어노테이션 추가

--- thymeleaf/src/main/java/io/github/seondongpyo/thymeleaf/web/domain/MemberRegisterForm.java
+++ thymeleaf/src/main/java/io/github/seondongpyo/thymeleaf/web/domain/MemberRegisterForm.java
@@ -3,12 +3,19 @@ package io.github.seondongpyo.thymeleaf.web.domain;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotBlank;
+
 @Getter
 @Setter
 public class MemberRegisterForm {
 
+    @NotBlank(message = "이름을 입력하세요.")
     private String name;
+
+    @NotBlank(message = "나이를 입력하세요.")
     private Integer age;
+
+    @NotBlank(message = "권한을 선택하세요.")
     private Role role;
 
     public Member toEntity() {
