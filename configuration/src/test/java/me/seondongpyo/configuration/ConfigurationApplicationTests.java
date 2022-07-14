06be9da673c272e9e21158fbe530e46c732fe3ca
seondongpyo <seondongpyo@gmail.com>
2022-07-14 23:38:13 +0900

    feat: Spring configuration 기능을 학습하기 위한 테스트 프로젝트 생성

---
+++ configuration/src/test/java/me/seondongpyo/configuration/ConfigurationApplicationTests.java
@@ -0,0 +1,13 @@
+package me.seondongpyo.configuration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigurationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
